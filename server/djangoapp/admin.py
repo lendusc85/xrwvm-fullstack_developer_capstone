d4d789b6d4da0c8fa3bcc2e58c84357f098344dc
Lendus Casey <lcasey@mauijim.com>
2024-09-24 14:41:49 -0500

    finished car-models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'year', 'car_make', 'price', 'fuel_type')
+    list_filter = ('type', 'year', 'fuel_type')
+    search_fields = ('name', 'car_make__name')
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_of_origin', 'founded_year')
+    search_fields = ('name', 'country_of_origin')
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,18 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country_of_origin = models.CharField(max_length=100, blank=True, null=True)
+    founded_year = models.IntegerField(blank=True, null=True, validators=[
+        MinValueValidator(1800),
+        MaxValueValidator(2023)
+    ])
+    logo = models.ImageField(upload_to='car_makes_logos/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.country_of_origin})" if self.country_of_origin else self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +35,39 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CONVERTABLE', 'Convertable'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+    ]
+    type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    fuel_type = models.CharField(
+        max_length=20,
+        choices=[
+            ('Gasoline', 'Gasoline'),
+            ('Diesel', 'Diesel'),
+            ('Electric', 'Electric'),
+            ('Hybrid', 'Hybrid')
+        ],
+        default='Gasoline'
+    )
+    engine = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.type} ({self.year})"
+    
